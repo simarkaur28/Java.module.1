787e9fe215614b1586f738aa790cdf97e71c99aa
simarkaur28 <kaursimar0028@gmail.com>
2021-09-07 15:06:39 +0530

    initial commit

--- src/example.java
+++ src/example.java
@@ -1,2 +1,11 @@
 public class example {
+    public static void main(String args[])
+    {
+        int num=10;
+        System.out.println("num="+num); //system class, out object
+        num=2*num ;
+        system.out.println(num);
+    }
 }
+
+//javac = java compiler
