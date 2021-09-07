ada22df116d1287f80a534e4873e311613804404
simarkaur28 <kaursimar0028@gmail.com>
2021-09-07 15:10:58 +0530

    initial commit

--- src/example.java
+++ src/example.java
@@ -1,10 +1,11 @@
 public class example {
-    public static void main(String args[])
+    public static void main(String[] args)
     {
         int num=10;
         System.out.println("num="+num); //system class, out object
         num=2*num ;
-        system.out.println(num);
+        System.out.println("Value of num is multiplied by 2");
+        System.out.println(num);
     }
 }
 
